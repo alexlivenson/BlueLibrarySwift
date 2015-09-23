8955d8e02e93d5ed73819cbf0a787d29c8f5c801
alex livenson <alexlivenson1@n003-000-000-000.static.ge.com>
2015-09-23 09:28:37 -0700

    Initial Commit

---
+++ BlueLibrarySwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BlueLibrarySwift
+//
+//  Created by alex livenson on 9/23/15.
+//  Copyright © 2015 alex livenson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
