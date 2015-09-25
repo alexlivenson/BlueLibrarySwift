58350e060655c5bd2f43ee42e38e197189df1fc2
alex livenson <alexlivenson1@alexs-MacBook-Pro.local>
2015-09-25 11:28:58 -0700

    Added play ground

---
+++ BlueLibrarySwift/BlueLibraryPlayground.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+var str = "Hello, playground"
+
+var arr = [Int]()
+arr.append(1)
+arr.append(2)
+arr.append(3)
+
+arr.popLast()
+arr.popLast()
+arr
